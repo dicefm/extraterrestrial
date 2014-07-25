dfff39bc7e0207e95013d1162c73d1af5f0a396a
Simon Richardson <simon@ustwo.co.uk>
2014-07-25 15:50:55 +0100

    Adding all the test cases

--- normaliser_test.go
+++ normaliser_test.go
@@ -89,3 +89,105 @@ func Test_Phone_USA_HKG_missmatch(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func Test_Phone_invalid_HKG(t *testing.T) {
+	makeNum := func(a uint) string {
+		mod := a % 10
+		return fmt.Sprintf("+1(817) 5%v9-890%v", mod, mod)
+	}
+
+	f := func(x uint) error {
+		return ErrNotFound
+	}
+	g := func(x uint) error {
+		_, err := Normalise(makeNum(x), "HKG")
+		return err
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Phone_valid_USA(t *testing.T) {
+	makeNum := func(a uint) string {
+		mod := a % 10
+		return fmt.Sprintf("+1(817) 5%v9-890%v", mod, mod)
+	}
+	prepare := func(a string) string {
+		res, _ := normalisePhoneNumber(a)
+		return fmt.Sprintf("+%s", res)
+	}
+
+	f := func(x uint) *PhoneResult {
+		return NewPhoneResult(prepare(makeNum(x)), "USA")
+	}
+	g := func(x uint) *PhoneResult {
+		res, _ := Normalise(makeNum(x), "")
+		return res
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Phone_valid_USA_2(t *testing.T) {
+	makeNum := func(a uint) string {
+		mod := a % 10
+		return fmt.Sprintf("(817) 5%v9-890%v", mod, mod)
+	}
+	prepare := func(a string) string {
+		res, _ := normalisePhoneNumber(a)
+		return fmt.Sprintf("+1%s", res)
+	}
+
+	f := func(x uint) *PhoneResult {
+		return NewPhoneResult(prepare(makeNum(x)), "USA")
+	}
+	g := func(x uint) *PhoneResult {
+		res, _ := Normalise(makeNum(x), "")
+		return res
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Phone_invalid_USA_3(t *testing.T) {
+	makeNum := func(a uint) string {
+		mod := a % 10
+		return fmt.Sprintf("6123-61%v%v", mod, mod)
+	}
+
+	f := func(x uint) error {
+		return ErrNotFound
+	}
+	g := func(x uint) error {
+		_, err := Normalise(makeNum(x), "")
+		return err
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Phone_valid_USA_3(t *testing.T) {
+	makeNum := func(a uint) string {
+		mod := a % 10
+		return fmt.Sprintf("6123-61%v%v", mod, mod)
+	}
+	prepare := func(a string) string {
+		res, _ := normalisePhoneNumber(a)
+		return fmt.Sprintf("+852%s", res)
+	}
+
+	f := func(x uint) *PhoneResult {
+		return NewPhoneResult(prepare(makeNum(x)), "HKG")
+	}
+	g := func(x uint) *PhoneResult {
+		res, _ := Normalise(makeNum(x), "HKG")
+		return res
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
