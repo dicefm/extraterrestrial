1af7ee15c8171d4d9ff269bab808bffece745d1c
Gregoire Lejay <gregoire.lejay@gmail.com>
2015-01-27 10:49:45 +0000

    tr

--- lib/bash/unit-test.sh
+++ lib/bash/unit-test.sh
@@ -1,1 +1,1 @@
-#!/bin/shgo test -p 1 `find . -mindepth 1 -type d -exec sh -c 'ls -1 "{}"|egrep -i -q "_test\.go$"' ';' -print | tr -d '\r'`
+#!/bin/shgo test -p 1 `find . -mindepth 1 -type d -exec sh -c 'ls -1 "{}"|egrep -i -q "_test\.go$"' ';' -print'
