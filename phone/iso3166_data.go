02908d0fa33536a922fd2003ae0a07647f2ad0d8
Hugh Cannon <hugh@hughcannon.com>
2018-11-05 11:31:07 +0000

    Add Malta 77

--- phone/iso3166_data.go
+++ phone/iso3166_data.go
@@ -668,7 +668,7 @@ var (
 		},
 		&PhoneData{
 			CountryData:        c.ISO3166_CountriesData[c.CountryNameMalta],
-			MobileBeginsWith:   []string{"79", "99"},
+			MobileBeginsWith:   []string{"77", "79", "99"},
 			PhoneNumberLengths: []int{8},
 		},
 		&PhoneData{
